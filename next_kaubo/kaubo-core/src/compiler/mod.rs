3d56e12a90afafd5efde8dcab2a24fbff565a043
nyml2003 <2492181618@qq.com>
2026-02-20 18:19:46 +0800

    feat: 删除kaubo-core

--- next_kaubo/kaubo-core/src/compiler/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod lexer;
-pub mod module;
-pub mod parser;
--- next_kaubo/kaubo-core/src/compiler/parser/module.rs
+++
@@ -1,8 +0,0 @@
-use super::stmt::Stmt;
-
-#[derive(Debug, Clone)]
-pub struct ModuleKind {
-    pub statements: Vec<Stmt>,
-}
-
-pub type Module = Box<ModuleKind>;
